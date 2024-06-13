c6b52bfd24290708114488e775995ebd7f25073f
Revaz Sologhashvili <rsologhashvili@evex.ge>
2024-06-13 18:20:57 +0400

    fixing errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import SocialLinksCard from "./Components/SocialLinksCard";
 
 function App() {
   return (
-    <main className="bg-DarkGray h-screen  flex justify-center items-center w-screen p-6 ">
+    <main className=" bg-Black h-screen  flex justify-center items-center w-screen p-6 ">
       <SocialLinksCard />
     </main>
   );
--- src/Components/CardHeading.tsx
+++ src/Components/CardHeading.tsx
@@ -6,7 +6,7 @@ interface HeadingProps {
 const CardHeading = (props: HeadingProps) => {
   return (
     <div className="text-center">
-      <h1 className="text-3xl text-white font-bold mt-6">{props.name}</h1>
+      <h1 className="text-2xl text-white font-bold mt-6">{props.name}</h1>
       <p className="text-NeonGreen text-sm font-bold">{props.location}</p>
       <p className="text-sm my-6 text-white">"{props.jobTitle}"</p>
     </div>
